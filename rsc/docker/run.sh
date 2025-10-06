51563c7693347ac5c789cd518f44e226717e4981
nfour <nfour@3C7.42angouleme.fr>
2025-10-06 12:42:18 +0200

    Fix run docker

--- rsc/docker/run.sh
+++ rsc/docker/run.sh
@@ -5,7 +5,7 @@ ROOT_DIR=$(pwd)
 docker build -t ft_lex ${ROOT_DIR}/rsc/docker
 GIT_EMAIL=$(git config user.email)
 GIT_NAME=$(git config user.name)
-docker run -it -e GIT_EMAIL=$(git config user.email) -e GIT_NAME=$(git config user.name) --rm -v ${ROOT_DIR}:/app --workdir /app ft_lex\
+docker run -it -v ~/.ssh/:/root/.ssh/ --rm -v ${ROOT_DIR}:/app --workdir /app ft_lex\
     /bin/bash -c "git config --global user.email ${GIT_EMAIL} &&\
     git config --global user.name ${GIT_NAME} &&\
     /bin/zsh"
